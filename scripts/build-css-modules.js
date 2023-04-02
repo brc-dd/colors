5bf829702bf79154b75c8a9ca4c4b3c049661b41
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-04-02 19:31:48 +0530

    chore: generate minified bundle and update dark class

--- scripts/build-css-modules.js
+++ scripts/build-css-modules.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const postcss = require('postcss');
 const allColorScales = require('../index');
 
 const outputDir = require('../tsconfig.json').compilerOptions.outDir;
+const output = {};
 
 Object.entries(allColorScales).forEach(([colorScaleName, scale]) => {
-  const selector = /DarkA?$/.test(colorScaleName) ? '.dark-theme' : ':root';
+  const selector = /DarkA?$/.test(colorScaleName) ? '.dark' : ':root';
   const scaleAssCssProperties = Object.entries(scale)
-    .map(([name, value]) => `  --${name}: ${value};`)
+    .map(([name, value]) => `--${name}: ${value};`)
     .join('\n');
-  const scaleAsCssFile = `${selector} {\n${scaleAssCssProperties}\n}`;
-  fs.writeFileSync(
-    path.join(outputDir, colorScaleName + '.css'),
-    scaleAsCssFile
-  );
+  output[selector] = (output[selector] || '') + scaleAssCssProperties;
 });
+
+postcss([require('cssnano')({ preset: 'default' })])
+  .process(`:root{${output[':root']}}\n.dark{${output['.dark']}}`, {
+    from: undefined
+  })
+  .then((result) => {
+    fs.writeFileSync(path.join(outputDir, 'index.min.css'), result.css);
+  });
